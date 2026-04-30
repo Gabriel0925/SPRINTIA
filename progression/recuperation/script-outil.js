8cf74798cf203b31c51b01d5ed6c086a8cfbea01
Gabriel0925 <gabchap486@gmail.com>
2026-04-30 10:47:45 +0200

    Fix bugs

--- progression/recuperation/script-outil.js
+++ progression/recuperation/script-outil.js
@@ -2,3 +2,17 @@ async function saveRecuperation() {
     alert("Pas encore disponible")
     return
 }
+
+function init() {
+    let zoneReponseCoach = document.getElementById("reponse-coach")
+    let interpretation = "Pour analyser ta récupération, j'ai besoin de 7 jours d'affiler de données pour pouvoir."
+
+    zoneReponseCoach.innerHTML = interpretation
+
+    // generation du graphique
+    genererGraphiqueLine(["L", "M", "M", "J", "V"], [45, 47, 49, 46, 43])
+
+    return
+}
+
+window.addEventListener("DOMContentLoaded", init())

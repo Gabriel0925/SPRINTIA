9413a827a89f6331eef833c89386d4d30f61d792
Gabriel0925 <gabchap486@gmail.com>
2026-05-12 19:19:34 +0200

    Added a new style for navbar

--- progression/predicteur-performance/script-outil.js
+++ progression/predicteur-performance/script-outil.js
@@ -1,4 +1,7 @@
-function estimateTime(vmaUser, profilUser) {
+// variable globale pour savoir si le user a mis une distance perso
+let userAddDistance = false
+
+function estimateTime(vmaUser, profilUser, distanceUser) {
     // init
     const dicoCoef = {
         "sprint":[1, 0.88, 0.78, 0.71, 0.64],
@@ -12,12 +15,29 @@ function estimateTime(vmaUser, profilUser) {
     let time1km = 1/(vmaUser*dicoCoef[profilUser][0])
     let time5km = 5/(vmaUser*dicoCoef[profilUser][1])
     let time10km = 10/(vmaUser*dicoCoef[profilUser][2])
-    let timeSemiMarathon = 21/(vmaUser*dicoCoef[profilUser][3])
-    let timeMarathon = 42/(vmaUser*dicoCoef[profilUser][4])
+    let timeSemiMarathon = 21.0975/(vmaUser*dicoCoef[profilUser][3])
+    let timeMarathon = 42.195/(vmaUser*dicoCoef[profilUser][4])
 
     const tableauTime = [time400m*60, time800m*60, time1km*60, time5km*60, time10km*60, timeSemiMarathon*60, timeMarathon*60] // conversion des secondes en minutes
+    if (distanceUser != undefined) {
+        let indexCoef = 0
+        if (distanceUser <= 2) {
+            indexCoef=0
+        } else if (distanceUser <= 5.0) {
+            indexCoef=1
+        } else if (distanceUser <= 10.0) {
+            indexCoef=2
+        } else if (distanceUser <= 22.0) {
+            indexCoef=3
+        } else {
+            indexCoef=4
+        }
+        
+        let timePersonaliser = distanceUser/(vmaUser*dicoCoef[profilUser][indexCoef])
+        tableauTime.push(timePersonaliser*60)
+    }
+    
     let tableauTimePredict = []
-
     tableauTime.forEach(element => {
         tableauTimePredict.push(dureeFormatee(element, null))
     });
@@ -32,7 +52,7 @@ function predictorTimeRunning() {
 
     // vérification des champs
     if (isNaN(vmaUser)) {
-        alert("Erreur de saisie : tous les champs doivent être remplis.");
+        alert("Erreur de saisie : le champ VMA doit être remplis.");
         return
     }
     if (vmaUser <= 0) {
@@ -43,8 +63,30 @@ function predictorTimeRunning() {
         alert("Valeur non valide, la vma doit être inférieur à 50.")
         return
     }
-
-    let tableauResultTime = estimateTime(vmaUser, profilUser)
+    
+    // si le user a ajouté une distance personnaliser
+    let tableauResultTime=undefined
+    if (userAddDistance==true) {
+        let distanceUser = parseFloat(document.getElementById("distance-user").value)
+        
+        if (isNaN(distanceUser)) {
+            alert("Erreur de saisie : le champ Distance doit être remplis.");
+            return
+        }
+        if (distanceUser <= 0) {
+            alert("Valeur non valide, la distance doit être supérieur à 0.")
+            return
+        }
+        if (distanceUser >= 50) {
+            alert("Valeur non valide, la distance doit être inférieur à 50.")
+            return
+        }
+        
+        tableauResultTime = estimateTime(vmaUser, profilUser, distanceUser)
+    } else {
+        tableauResultTime = estimateTime(vmaUser, profilUser, undefined)
+    }
+    
     let blockData = document.querySelectorAll(".container-block-data-data")
 
     let compteur = 0
@@ -54,6 +96,24 @@ function predictorTimeRunning() {
     });
 
     return
+}
+
+function distancePersonnaliser() {
+    let buttonDistance = document.getElementById("button-distance")
+    let inputDistance = document.getElementById("input-distance")
+    let blockDistancePerso = document.getElementById("block-distance-personnaliser")
+    
+    if (buttonDistance.textContent == "Ajouter une distance") {
+        buttonDistance.textContent = "Supprimer la distance"
+        inputDistance.style.display = "block"
+        blockDistancePerso.style.display = "block"
+        userAddDistance = true
+    } else {
+        buttonDistance.textContent = "Ajouter une distance"
+        inputDistance.style.display = "none"
+        blockDistancePerso.style.display = "none"
+        userAddDistance = false
+    }
 }
 
 function forSelect() {

8f2f1f563138feb070f579cccbddf6f9136fb286
Gabriel0925 <gabchap486@gmail.com>
2026-06-14 16:38:12 +0200

    Commit SPRINTIA 5 beta - 14J263 (9)

---
+++ progression/niveau-course/script-historique-niveau-course.js
@@ -0,0 +1,62 @@
+function ReturnDate(DateNiveauCourse) {
+    let DateEuropeen = ""
+
+    DateNiveauCourse = DateNiveauCourse.split("-")
+    // Inversion de la date de "2026-01-12" à "12-01-2026"
+    DateEuropeen = DateNiveauCourse[2] + "-" + DateNiveauCourse[1] + "-" + DateNiveauCourse[0]
+    return DateEuropeen
+}
+async function RecupValueNiveauCourseGraphique() {
+    // Initialisation 
+    let TailleHardware = window.innerWidth
+    let NbValeurRecup = -13
+
+    // Logique de nb datas en fonction du devices
+    if (TailleHardware <= 520) {
+        NbValeurRecup = -4
+    } else if (TailleHardware <= 640) {
+        NbValeurRecup = -6
+    } else if (TailleHardware <= 720) {
+        NbValeurRecup = -7
+    } else if (TailleHardware <= 790) {
+        NbValeurRecup = -8
+    } else if (TailleHardware <= 1000) {
+        NbValeurRecup = -10
+    }
+
+    // Recup value Data
+    const ValeurDB = await db.niveau_course.toArray()    
+    
+    // Trier par date 
+    ValeurDB.sort((element1, element2) => { // En js on peut comparer 2 dates comme des maths
+        if (element1.date < element2.date) return -1
+        if (element1.date > element2.date) return 1
+    })
+
+    // map permet de retourner une nouvelle liste a partir d'une premiere liste et de prendre qu'une seule clé d'un objet
+    // slice permet de découper un tableau pour en garder qu'une partie grace aux indices
+    const NiveauDatas = ValeurDB.slice(NbValeurRecup).map(dataBDD => dataBDD.niveau_course_user) // -10 pr prendre les 1à dernieres valeur
+    const DateDatas = ValeurDB.slice(NbValeurRecup).map(dataBDD => dataBDD.date)
+
+    // Initialisation d'une liste de date avec le format européen
+    let ListeDate = []
+    let DateEuropeen = ""
+
+    DateDatas.forEach(element => { // Parcours des dates
+        DateEuropeen = ReturnDate(element)
+        ListeDate.push(DateEuropeen) // Ajout à la liste des dates format européen
+    });
+    
+    return {NiveauDatas, ListeDate}
+}
+async function graph() {
+    // attendre la recup des datas
+    let {NiveauDatas, ListeDate} = await RecupValueNiveauCourseGraphique()
+
+    if (NiveauDatas.length <= 0) {
+        NiveauDatas = [0, 0, 0]
+        ListeDate = ["Janvier", "Février", "Mars"]
+    }
+    
+    genererGraphiqueLine(ListeDate, NiveauDatas)
+}

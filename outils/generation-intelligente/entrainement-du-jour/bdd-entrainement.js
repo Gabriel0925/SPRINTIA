f0ed992f0fadb0c4296fb39790bcb1b57ce782e1
Gabriel0925 <gabchap486@gmail.com>
2026-07-16 14:43:51 +0200

    Release SPRINTIA 5.2 beta - 16J261 (9)

---
+++ outils/generation-intelligente/entrainement-du-jour/bdd-entrainement.js
@@ -0,0 +1,65 @@
+const bddSeancesCourseFacile = {
+    "20": [
+        {
+            "structure": {
+                "echauffement": {
+                    "duree": [5, "min"]
+                },
+                "fractionne": {
+                    "nombre_repetitions": 10,
+                    "duree_repetition": [40, "sec"],
+                    "duree_recuperation": [20, "sec"]
+                },
+                "retour_au_calme": {
+                    "duree": [5, "min"]
+                }
+            },
+            "type": "fractionne",
+            "lien": "google.com"
+        }
+    ],
+    "40": "",
+    "60": ""
+}
+const bddSeancesCourseModere = {
+    "20": "",
+    "40": "",
+    "60": ""
+}
+const bddSeancesCourseDifficile = {
+    "20": "",
+    "40": "",
+    "60": ""
+}
+
+const bddSeancesVeloFacile = {
+    "20": "",
+    "40": "",
+    "60": ""
+}
+const bddSeancesVeloModere = {
+    "20": "",
+    "40": "",
+    "60": ""
+}
+const bddSeancesVeloDifficile = {
+    "20": "",
+    "40": "",
+    "60": ""
+}
+
+const bddSeancesNatationFacile = {
+    "20": "",
+    "40": "",
+    "60": ""
+}
+const bddSeancesNatationModere = {
+    "20": "",
+    "40": "",
+    "60": ""
+}
+const bddSeancesNatationDifficile = {
+    "20": "",
+    "40": "",
+    "60": ""
+}
--- outils/generation-intelligente/entrainement-du-jour/script-entrainement-du-jour.js
+++ outils/generation-intelligente/entrainement-du-jour/script-entrainement-du-jour.js
@@ -9,13 +9,36 @@ function selectItem(elt, nameComponent) {
     }   
 }
 
+const bddSeances = {
+    "course": {
+        "facile": bddSeancesCourseFacile, 
+        "modere": bddSeancesCourseModere, 
+        "difficile": bddSeancesCourseDifficile, 
+    },
+    "velo": {
+        "facile": bddSeancesVeloFacile, 
+        "modere": bddSeancesVeloModere, 
+        "difficile": bddSeancesVeloDifficile, 
+    },
+    "natation": {
+        "facile": bddSeancesNatationFacile, 
+        "modere": bddSeancesNatationModere, 
+        "difficile": bddSeancesNatationDifficile, 
+    }
+}
 async function generationWorkout() {
     let button = document.getElementById("button-generation-entrainements")
     button.disabled = true
     button.textContent = "Génération..."
 
     try {
-        
+        const sportChoice = document.querySelector(".sport-item.selected").id
+        const dureeChoice = document.querySelector(".round-input-item.duree.selected").id
+        const intensityChoice = document.querySelector(".round-input-item.intensity.selected").id
+
+        const bddWorkout = bddSeances[sportChoice][intensityChoice] // on récupère le dico des séances qui se trouve dans le fichier 'bdd-entrainement.js'
+
+
         button.textContent = "Généré"
         await new Promise(transmissionInfoUser => setTimeout(transmissionInfoUser, 500))
         //window.location.href = ``

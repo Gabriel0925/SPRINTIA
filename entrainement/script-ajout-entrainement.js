1663b87f183066716f3f3e2a269e5c8f26ca5dd6
Gabriel0925 <gabchap486@gmail.com>
2026-08-27 11:35:39 +0200

    Optimisation

--- entrainement/script-ajout-entrainement.js
+++ entrainement/script-ajout-entrainement.js
@@ -75,6 +75,12 @@ async function VerificationParam() {
                     9:"Effort maximal",
                     10:"Effort maximal"
                 }
+                const dicoDescriptionRPE = {
+                    1:["Facile", "#1fff80"], 2:["Facile", "#1fff80"], 3:["Facile", "#1fff80"],
+                    4:["Modéré", "#e7e625"], 5:["Modéré", "#e7e625"], 6:["Modéré", "#e7e625"],
+                    7:["Difficile", "#ff4b4c"], 8:["Difficile", "#ff4b4c"],
+                    9:["Effort maximal", "#9386f1"], 10:["Effort maximal", "#9386f1"]
+                }
 
                 // remplissage des champs qu'on ne peut pas remplir dans la boucle foreach
                 document.getElementById("profil-sport").value = WorkoutDB.sport
@@ -82,7 +88,9 @@ async function VerificationParam() {
                 // remettre le RPE sur bonne position
                 document.querySelector(".slider input").value = WorkoutDB.rpe
                 document.querySelector(".slider progress").value = WorkoutDB.rpe
-                document.querySelector(".slider-name").innerHTML = "RPE : <span class='RPE'>" + WorkoutDB.rpe + "</span>" + dicoRPE[WorkoutDB.rpe]
+                document.querySelector(".slider-name").innerHTML = "RPE : <span class='RPE'>" + WorkoutDB.rpe + "</span>" + " " + "<strong>" + dicoDescriptionRPE[WorkoutDB.rpe][0] + "</strong>"
+                document.querySelector(".slider-name span.RPE").style.background = dicoDescriptionRPE[WorkoutDB.rpe][1]
+                document.querySelector(".slider-name strong").style.color = dicoDescriptionRPE[WorkoutDB.rpe][1]
 
                 // ajout d'une data a la variable globale
                 if (WorkoutDB.note != undefined && WorkoutDB.note != "") {

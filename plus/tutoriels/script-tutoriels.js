ea90776f58fa95d123daaf0b3c3af18041ea41b0
Gabriel0925 <gabchap486@gmail.com>
2026-09-17 20:07:50 +0200

    opti skeleton

--- plus/tutoriels/script-tutoriels.js
+++ plus/tutoriels/script-tutoriels.js
@@ -1,4 +1,7 @@
 window.addEventListener("load", () => {
     let divChargement = document.querySelector(".container-chargement")
     if (divChargement) {divChargement.classList.add("finish")}
+
+    let mainLoading = document.querySelector("main.loading")
+    if (mainLoading) {mainLoading.classList.remove("loading")}
 })

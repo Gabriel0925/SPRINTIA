0b85a7497e9259e13557d85a74a297b49ede422c
Gabriel0925 <gabchap486@gmail.com>
2026-09-19 16:24:58 +0200

    fix bugs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION_CACHE = "V1.1.12" 
+const VERSION_CACHE = "V1.1.13" 
 // tous les fichiers qu'on glisse dans le cache pour le mode hors ligne
 const fileInCache = [
     "/",
@@ -125,12 +125,14 @@ const fileInCache = [
     "/progression/charge-entrainement/charge-entrainement.html", "/progression/charge-entrainement/script-charge-entrainement.js",
     
     "/progression/indulgence-course/indulgence-course.html", "/progression/indulgence-course/parametres.html", "/progression/indulgence-course/script-outil.js",
+    "/progression/indulgence-course/script-parametres.js",
 
     "/progression/niveau-course/a-propos.html", "/progression/niveau-course/ajouter-niveau-course.html", "/progression/niveau-course/niveau-course-analyse.html", "/progression/niveau-course/niveau-course-evolution.html", 
     "/progression/niveau-course/script-historique-niveau-course.js", "/progression/niveau-course/script-niveau-course-analyse.js", "/progression/niveau-course/script-sauvegarder-niveau.js",
 
     "/progression/recuperation/ajouter-recuperation.html", "/progression/recuperation/historique-recuperation.html",
-    "/progression/recuperation/recuperation.html", "/progression/recuperation/script-recuperation.js",
+    "/progression/recuperation/recuperation.html", "/progression/recuperation/ajouter-recuperation.js", "/progression/recuperation/script-historique.js",
+    "/progression/recuperation/script-recuperation.js",
 
     "/index.html", "/index.js","/manifest.json"
 ]
